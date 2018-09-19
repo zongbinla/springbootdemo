12b934627ba904cda6f7e8519da2f5e39112e348
zongbinla <1025039936@qq.com>
2018-09-19 08:30:48 +0800

    Initial commit

---
+++ src/main/java/Example.java
@@ -0,0 +1,20 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Zongbin on 2018/9/18.
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+}
